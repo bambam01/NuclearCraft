716dbdeaaea8e8ef75d1ca0ce81d8fad86488cd6
DrParadox7 <47131096+DrParadox7@users.noreply.github.com>
2021-07-18 16:33:56 +0100

    IC2 tweaks
    Removes the creation of certain NC crafting parts

--- src/main/java/nc/IC2Recipes.java
+++ src/main/java/nc/IC2Recipes.java
@@ -26,8 +26,8 @@ public final class IC2Recipes {
 		try {RecipesHelp.addMFAmplifier(new ItemStack (NCItems.parts, 1, 4), 1, 16000);} catch(Exception e) {}
 		
 		// Enriching
-		try {Recipes.cannerEnrich.addRecipe(FluidRegistry.getFluidStack(FluidRegistry.WATER.getName(), 1000), new RecipeInputOreDict("universalReactant", 8), FluidRegistry.getFluidStack("ic2coolant", 1000));} catch(Exception e) {}
-		try {Recipes.cannerEnrich.addRecipe(FluidRegistry.getFluidStack("distilledWater", 1000), new RecipeInputOreDict("universalReactant", 1), FluidRegistry.getFluidStack("ic2coolant", 1000));} catch(Exception e) {}
+		// try {Recipes.cannerEnrich.addRecipe(FluidRegistry.getFluidStack(FluidRegistry.WATER.getName(), 1000), new RecipeInputOreDict("universalReactant", 8), FluidRegistry.getFluidStack("ic2coolant", 1000));} catch(Exception e) {}
+		// try {Recipes.cannerEnrich.addRecipe(FluidRegistry.getFluidStack("distilledWater", 1000), new RecipeInputOreDict("universalReactant", 1), FluidRegistry.getFluidStack("ic2coolant", 1000));} catch(Exception e) {}
 		
 		// Macerator
 		try {RecipesHelp.addMaceratorRecipe("oreThorium", 1, new ItemStack(NCItems.material, 2, 20));} catch(Exception e) {}
@@ -44,8 +44,8 @@ public final class IC2Recipes {
 		try {RecipesHelp.addMaceratorRecipe("ingotMagnesiumDiboride", 1, new ItemStack(NCItems.material, 1, 72));} catch(Exception e) {}
 		
 		// Compressor
-		try {RecipesHelp.addCompressorRecipe(new ItemStack(NCItems.parts, 1, 0), NuclearCraft.workspace ? 4 : 8, new ItemStack(NCItems.parts, 1, 3));} catch(Exception e) {}
-		try {RecipesHelp.addCompressorRecipe("ingotTough", 1, new ItemStack(NCItems.parts, 2, 0));} catch(Exception e) {}
+		// try {RecipesHelp.addCompressorRecipe(new ItemStack(NCItems.parts, 1, 0), NuclearCraft.workspace ? 4 : 8, new ItemStack(NCItems.parts, 1, 3));} catch(Exception e) {}
+		// try {RecipesHelp.addCompressorRecipe("ingotTough", 1, new ItemStack(NCItems.parts, 2, 0));} catch(Exception e) {}
 
 		// Thermal Centrifuge
 		try {RecipesHelp.addCentrifugeRecipe(new RecipeInputOreDict("ingotLithium", 1), 250, new ItemStack(NCItems.material, 1, 47), new ItemStack(NCItems.material, 1, 69));} catch(Exception e) {}
