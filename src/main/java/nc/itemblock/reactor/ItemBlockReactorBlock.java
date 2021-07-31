fbb893cf8485111d1b77cacaf30037578ab0fe1c
Tom <bambam0100@gmail.com>
2021-07-31 02:28:49 +0200

    formatting

--- src/main/java/nc/itemblock/reactor/ItemBlockReactorBlock.java
+++ src/main/java/nc/itemblock/reactor/ItemBlockReactorBlock.java
@@ -1,19 +1,18 @@
 package nc.itemblock.reactor;
 
 import nc.itemblock.ItemBlockMeta;
-import nc.itemblock.ItemBlockNC;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 
 public class ItemBlockReactorBlock extends ItemBlockMeta {
 
-	public ItemBlockReactorBlock(Block block) {
-		super(block);
-	}
+    public ItemBlockReactorBlock(Block block) {
+        super(block);
+    }
 
 
-	@Override
-	public String getUnlocalizedName(ItemStack itemstack) {
-		return this.getUnlocalizedName() + "_" + itemstack.getItemDamage();
-	}
+    @Override
+    public String getUnlocalizedName(ItemStack itemstack) {
+        return this.getUnlocalizedName() + "_" + itemstack.getItemDamage();
+    }
 }
